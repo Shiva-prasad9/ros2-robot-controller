f30748dcf4e2b73bb49d7652de351090941380bd
bn_shiva <allforshivu@gmail.com>
2026-08-17 21:45:02 +0200

    chore: ignore colcon build artifacts, add .gitignore

--- build/my_robot_localization/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bn_shiva/ros2_ws/src/my_robot_localization
-# Build directory: /home/bn_shiva/ros2_ws/src/build/my_robot_localization
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
